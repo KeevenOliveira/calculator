a78915301811410a5b74ce06fa7601df7c6c7648
Keeven Oliveira <keevinhuholiveira@gmail.com>
2024-09-29 21:32:34 -0300

    [FIX]: Add adjusts in unit tests of Equal component

--- src/components/Equal/Equal.spec.tsx
+++ src/components/Equal/Equal.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import Equal from ".";
 
 describe("<Equal/>", () => {
@@ -21,10 +21,12 @@ describe("<Equal/>", () => {
     render(<Equal setValue={mockSetValue} />);
     fireEvent.click(screen.getByText("="));
     const setValueCall = mockSetValue.mock.calls[0][0];
-    expect(setValueCall("2+2")).toBe("4");
-    expect(setValueCall("3*4")).toBe("12");
-    expect(setValueCall("10/2")).toBe("5");
-    expect(setValueCall("5-3")).toBe("2");
+    waitFor(() => {
+      expect(setValueCall("2+2")).toBe("4");
+      expect(setValueCall("3*4")).toBe("12");
+      expect(setValueCall("10/2")).toBe("5");
+      expect(setValueCall("5-3")).toBe("2");
+    });
   });
 
   it("handles invalid expressions", () => {
